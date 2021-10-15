bc463f5d29693cf323dd46744c5074a5b5a83211
Ahmed-Araby <ahmedaraby605@gmail.com>
2021-10-15 16:56:36 +0200

    send signal to the same process and handle it

---
+++ signals/signals_1.c
@@ -0,0 +1,34 @@
+/**
+ * process with id =1 is the init process
+ * which is the very first process in unix system
+ * 
+ * process can not handle kill signals
+ * 
+ * what I understood is that a handler only block the signal 
+ * temporary, then the signal effect get into action after 
+ * the handler return, but what happened here is that we got
+ * completly blocked the signal and changed the flow of the signal 
+ * action, we need to read linux sys programming book 
+ * 
+ * 
+ * man pages 
+ * https://man7.org/linux/man-pages/man2/kill.2.html
+ * https://man7.org/linux/man-pages/man2/signal.2.html
+ * */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handle_signal(int sig)
+{
+    printf("signal = %d \n", sig);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGIO, handle_signal);
+    int x = kill(getpid(), SIGIO);
+    printf("kil ret = %d \n", x);
+}
